9b88cc9c7cdc62b305d9ba084081aea1b514cf17
claudiokaan <claudiokaan@gmail.com>
2026-09-18 09:00:03 +0300

    feat: add harf yazimi default factory

--- js/modules/ilkokuma/index.js
+++ js/modules/ilkokuma/index.js
@@ -6,10 +6,15 @@
         return;
     }
 
-    // 1. Harf Yazımı (Sadece Metadata)
+    // 1. Harf Yazımı
     registry.register({
         id: "ilkokuma.harf_yazimi",
-        name: "Harf Yazımı"
+        name: "Harf Yazımı",
+        createDefault: function() {
+            return {
+                fontBoyutu: 80
+            };
+        }
     });
 
 })(typeof window !== 'undefined' && window.OkulZiliRegistry ? window.OkulZiliRegistry : null);
